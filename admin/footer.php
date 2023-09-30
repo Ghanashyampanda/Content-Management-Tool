25fb18615495189a569d8fe1d50925b7ae67cf17
Ghanashyam <ghanashyampanda07@gmail.com>
2023-09-30 11:47:07 +0530

    first commit

---
+++ admin/footer.php
@@ -0,0 +1,12 @@
+<!-- FOOTER -->
+      <footer class="main-footer" style="background-color: transparent;">
+ 
+        <div class="copyrights">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-4 text-center-md">
+                <p>&copy; Ghanashyam_Panda</p>
+              
+          </div>
+        </div>
+      </footer>
---
+++ footer.php
@@ -0,0 +1,61 @@
+<!-- Page Footer-->
+    <footer class="main-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="logo">
+              <h6 class="text-white">E-CodeSource</h6>
+            </div>
+            <div class="contact-details">
+              <p>259/A, Bhubhaneswar,Odisha,INDIA - 751002</p>
+              <p>Phone: (+91) 6370825302</p>
+              <p>Email: <a href="ghanashyampanda07@gmail.com">ghanashyampanda07@gmail.com</a></p>
+              <ul class="social-menu">
+                <li class="list-inline-item"><a href="#" target="_blank"><i class="fa fa-facebook-square"></i></a></li>
+                <li class="list-inline-item"><a href="#" target="_blank"><i class="fa fa-youtube"></i></a></li>
+              </ul>
+            </div>
+          </div>
+          <!--div class="col-md-4">
+            <div class="menus d-flex">
+              <ul class="list-unstyled">
+                <li> <a href="#">My Account</a></li>
+                <li> <a href="#">Add Listing</a></li>
+                <li> <a href="#">Pricing</a></li>
+                <li> <a href="#">Privacy &amp; Policy</a></li>
+              </ul>
+              <ul class="list-unstyled">
+                <li> <a href="#">Our Partners</a></li>
+                <li> <a href="#">FAQ</a></li>
+                <li> <a href="#">How It Works</a></li>
+                <li> <a href="#">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="latest-posts"><a href="#">
+                <div class="post d-flex align-items-center">
+                  <div class="image"><img src="img/small-thumbnail-1.jpg" alt="..." class="img-fluid"></div>
+                  <div class="title"><strong>Hotels for all budgets</strong><span class="date last-meta">October 26, 2016</span></div>
+                </div></a><a href="#">
+                <div class="post d-flex align-items-center">
+                  <div class="image"><img src="img/small-thumbnail-2.jpg" alt="..." class="img-fluid"></div>
+                  <div class="title"><strong>Great street atrs in London</strong><span class="date last-meta">October 26, 2016</span></div>
+                </div></a><a href="#">
+                <div class="post d-flex align-items-center">
+                  <div class="image"><img src="img/small-thumbnail-3.jpg" alt="..." class="img-fluid"></div>
+                  <div class="title"><strong>Best coffee shops in Sydney</strong><span class="date last-meta">October 26, 2016</span></div>
+                </div></a></div>
+          </div-->
+        </div>
+      </div>
+      <div class="copyrights">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <p>&copy; 2023. All rights reserved. by SourceCode and Projects.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
